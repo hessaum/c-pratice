ede13b84ba40506841ad13140214a1b2e2337ed5
hessaum <hessaum.sadri@gmail.com>
2017-02-24 17:44:21 -0800

    Trying to learn how to C

---
+++ first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+	num = 1;
+
+	printf("I am a simple");
+	printf("computer.\n");
+	printf("My favorite number is %d because it is first.\n",num);
+	printf("Daniel is my Based God");
+
+	return 0;
+
+}	
